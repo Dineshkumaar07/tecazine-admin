98923de1265a8b43b98000f6ef44c8e1938b6339
Dineshkumaar07 <dineshkumaar2003@gmail.com>
2024-07-27 16:18:52 +0530

    Added Loading Screen

--- src/pages/Viewer.jsx
+++ src/pages/Viewer.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 
 const Viewer = () => {
   const [documents, setDocuments] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     fetchDocuments();
@@ -14,6 +15,7 @@ const Viewer = () => {
       );
       const data = await response.json();
       setDocuments(data);
+      setLoading(false);
     } catch (error) {
       console.error("Error fetching documents:", error.message);
     }
@@ -26,25 +28,34 @@ const Viewer = () => {
   };
 
   return (
-    <div className="h-full w-screen flex flex-col p-5 font-poppins">
+    <div className="h-full flex flex-col p-5 font-poppins">
       <div className="">
         <h1 className="font-bold text-3xl  ">Tecazine Responses</h1>
-        <ul className="mt-9 ">
-          {documents.map((document) => (
-            <li
-              key={document}
-              className="hover:bg-gray-300 p-3 m-2 rounded-md flex items-center"
-            >
-              <div className="px-9 sm:w-1/2    ">{document} </div>
-              <button
-                onClick={() => downloadDocument(document)}
-                className="bg-purple-600 px-3 py-2 text-white rounded-lg hover:bg-"
+        {!loading && (
+          <ul className="mt-9">
+            {documents.map((document) => (
+              <li
+                key={document}
+                className=" p-3 m-2 rounded-md flex items-center cursor-pointer "
               >
-                Download
-              </button>
-            </li>
-          ))}
-        </ul>
+                <div className="px-9 flex sm:flex-row flex-col sm:items-center  w-full sm:w-1/2 sm:justify-between gap-3 border-2 p-2 rounded-md hover:bg-purple-200">
+                  {document}
+                  <button
+                    onClick={() => downloadDocument(document)}
+                    className="bg-purple-600 px-3 py-2 text-white rounded-lg "
+                  >
+                    Download
+                  </button>
+                </div>
+              </li>
+            ))}
+          </ul>
+        )}
+        {loading && (
+          <div className="h-[90svh] flex items-center justify-center">
+            Loading Responses.....
+          </div>
+        )}
       </div>
     </div>
   );
